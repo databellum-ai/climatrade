3ce3db66b76b0a961403f6ba4e50c8ac313796e4
JuanESuarez <prof02@yahoo.com>
2021-10-27 21:36:00 +0200

    upl

--- load_weatherNOAA.R
+++ load_weatherNOAA.R
@@ -21,14 +21,74 @@ station_data <- readRDS("./data/stations_NOAA.rds")
 
 
 
+# Define our target cities (to look for near stations near them)
+  # 10 ciudades que dirigen la economía mundial: https://cincodias.elpais.com/cincodias/2007/06/13/sentidos/1181701636_850215.html
+relevantCities <- data.frame(
+  id = c("NewYork", "Oviedo", "Amsterdam", "Paris", "HongKong", "Chicago", "London", "LosAngeles", "Beijng", "Madrid", "Albacete", "Frankfurt", "Tokyo", "Seoul", "Singapore", "Dubai"),
+  latitude = c(40.70623940806975, 43.3620683921967, 52.37288021193839, 48.8613182352403, 22.32029644568666, 41.88632188372439, 51.50702741724013, 34.05084926622552, 39.905384001792335, 40.425619645599916, 39.267266932791685, 50.12095925753092, 35.687667406759765, 37.568428507484775, 1.2929342653888358, 25.214919588761404), 
+  longitude = c(-74.00883633105707, -5.84817121485434, 4.896615844580131, 2.3003412809927495, 114.19091287611904, -87.67086062661967, -0.12701173276875632, -118.25389861960555, 116.37699181234836, -3.7025627487487984, -1.5500112927257998, 8.637929689001126, 139.76554769212072, 126.9780904050825, 103.84642630591777, 55.2762538818061))
+relevantCities
+
+
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~
 # https://rdrr.io/cran/rnoaa/man/
 tmp2 <- isd_stations(refresh = FALSE)
-
 tmp2
 
-isd_stations_search(lat = 38.4, lon = -123, radius = 250)
+
+
+station_data
+stationsPeriods <- station_data %>% group_by(id, first_year, last_year) %>% summarise()
+from_y <- 1989
+to_y <- 2021
+ 
+relevantCities[i,]
+c_radius <- 100
+vec[from_y:to_y] <- FALSE
+
+i <- 1
+i_city <- relevantCities$id[i]
+i_lat <- relevantCities$latitude[i]
+i_lon <- relevantCities$longitude[i]
+
+
+tmpAroundStations <- meteo_nearby_stations(lat_lon_df = relevantCities[i,], 
+                                     station_data = station_data,
+                                     radius = 50, 
+                                     limit = 10, 
+                                     var = c("PRCP", "TMIN", "TMAX"),
+                                     year_min = from_y, 
+                                     year_max = to_y)
+
+stationsPeriods
+tmpAroundStations <- as.tibble(tmpAroundStations[[1]]$id) %>% setnames("value", "id")
+tmpAroundStations
+tmpYearsCoveredPerNearbyStation <- 
+  tmpAroundStations %>% 
+  left_join(stationsPeriods)
+tmpYearsCoveredPerNearbyStation
+
+vec <- c(1:((to_y-from_y)+1))
+names(vec) <- c(from_y:to_y)
+vec[1:length(vec)] <- FALSE
+vec
+vec[1:length(vec)] <- FALSE
+vec["1990"] 
+vec
+sum(vec)
+
+
+
+
+
+
+
+tmpAroundStations <- isd_stations_search(lat = i_lat, lon = i_lon, radius = c_radius)
+
+beginDates <- ymd(as.character(tmpAroundStations$begin))
+endDates <- ymd(as.character(tmpAroundStations$end))
 
 monitors <- c("ASN00095063", "ASN00024025", "ASN00040112", "ASN00041023",
               "ASN00009998", "ASN00066078", "ASN00003069", "ASN00090162",
@@ -45,14 +105,6 @@ meteo_distance(station_data, -33, 151, radius = 10, limit = 10)
 
 
 
-
-# Define our target cities (to look for near stations near them)
-  # 10 ciudades que dirigen la economía mundial: https://cincodias.elpais.com/cincodias/2007/06/13/sentidos/1181701636_850215.html
-relevantCities <- data.frame(
-  id = c("NewYork", "Oviedo", "Amsterdam", "Paris", "HongKong", "Chicago", "London", "LosAngeles", "Beijng", "Madrid", "Albacete", "Frankfurt", "Tokyo", "Seoul", "Singapore", "Dubai"),
-  latitude = c(40.70623940806975, 43.3620683921967, 52.37288021193839, 48.8613182352403, 22.32029644568666, 41.88632188372439, 51.50702741724013, 34.05084926622552, 39.905384001792335, 40.425619645599916, 39.267266932791685, 50.12095925753092, 35.687667406759765, 37.568428507484775, 1.2929342653888358, 25.214919588761404), 
-  longitude = c(-74.00883633105707, -5.84817121485434, 4.896615844580131, 2.3003412809927495, 114.19091287611904, -87.67086062661967, -0.12701173276875632, -118.25389861960555, 116.37699181234836, -3.7025627487487984, -1.5500112927257998, 8.637929689001126, 139.76554769212072, 126.9780904050825, 103.84642630591777, 55.2762538818061))
-relevantCities
 
 # Obtain stations near our cities (<50km and up to 5 per city)
 tmpStations <- meteo_nearby_stations(lat_lon_df = relevantCities, 
