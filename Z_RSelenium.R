b1a85197a5b7e3fe6c487fa3d23b3a96ac5e2492
JuanESuarez <prof02@yahoo.com>
2021-11-22 12:18:08 +0100

    upl

--- Z_RSelenium.R
+++ Z_RSelenium.R
@@ -5,9 +5,17 @@
 
 # https://thatdatatho.com/tutorial-web-scraping-rselenium/
 
+library(tidyverse)
+library(rvest)
+
 library(RSelenium)
 library(binman)
-# system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE) 
+
+#Shut Down Client and Server
+# remote_driver$close()
+# driver$server$stop()
+# system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
+
 list_versions("chromedriver")
 driver <- rsDriver(version = "latest", browser=c("chrome"), chromever = "96.0.4664.45")
 remote_driver <- driver[["client"]]
@@ -40,6 +48,38 @@ tracksJustFound <-
 listenedTracks <- rbind(listenedTracks, tracksJustFound)
 head(listenedTracks)
 
+
+
+# ----------------------------------
+
+# <div data-testid="region-filter" class="RegionSelect__OverviewFilter-ibo3np-0 ilDAmP"><span class="Trigger-sc-18ecbf7-0 cGQwPM Trigger-uvfeg5-0 jQtrTN RegionSelect__StyledDropdownTrigger-ibo3np-1 iBVLW"><div class="Container-s1cmq-0 fQlZAH"><button aria-haspopup="listbox" id="dropdown-toggle" aria-labelledby="dropdown-label dropdown-toggle" aria-expanded="false" class="Button-sc-18tio8-0 jPCnoE">Global</button><svg role="img" focusable="false" height="16" width="16" viewBox="0 0 24 24" aria-hidden="true" class="Svg-sc-1usfroi-0 jhoCoB Arrow-sc-62daq7-0 dKKkZe"><polyline points="20 8 12 17 4 8" fill="none" stroke="#181818"></polyline></svg></div></span></div>
+
+# <span class="Trigger-sc-18ecbf7-0 cGQwPM Trigger-uvfeg5-0 jQtrTN RegionSelect__StyledDropdownTrigger-ibo3np-1 iBVLW"><div class="Container-s1cmq-0 fQlZAH"><button aria-haspopup="listbox" id="dropdown-toggle" aria-labelledby="dropdown-label dropdown-toggle" aria-expanded="false" class="Button-sc-18tio8-0 jPCnoE">Australia</button><svg role="img" focusable="false" height="16" width="16" viewBox="0 0 24 24" aria-hidden="true" class="Svg-sc-1usfroi-0 jhoCoB Arrow-sc-62daq7-0 dKKkZe"><polyline points="20 8 12 17 4 8" fill="none" stroke="#181818"></polyline></svg></div></span>
+
+
+test <- remote_driver$findElement(using = 'xpath', value = '//*[@id="dropdown-toggle"]')
+test <- remote_driver$findElement(using = 'class', value = 'Container-s1cmq-0')
+
+test <- remote_driver$findElement(using = 'xpath', value = '//*[@id="__next"]/div/div/main/div[2]/div[1]/header/div/div[2]/div/span/div')
+view(test$getElementText())
+
+
+test <- remote_driver$findElement(using = 'xpath', value = '//*[@id="__next"]/div/div/main/div[2]/div[1]/header/div/div[2]/div/span')
+test$clickElement()
+test$getActiveElement()
+
+element <- unlist(remote_driver$findElement("class name", "Button-sc-18tio8-0")$getElementAttribute('class'))
+length(element)
+
+
+test <- remote_driver$findElement("data-testid", "region-filter")
+test
+
+buttonElem <- remote_driver$findElement("class name", "Button-sc-18tio8-0")
+view(buttonElem$getElementText())
+buttonElem$clickElement()
+
+# -----------------------------------------
 
 
 address_element <- remote_driver$findElement(using = 'class', value = 'width70')
