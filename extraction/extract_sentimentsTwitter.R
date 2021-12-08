511bbff0baa091c02df3dc1cf3cf176e0dae7499
JuanESuarez <prof02@yahoo.com>
2021-12-08 13:05:47 +0100

    upl

--- extraction/extract_sentimentsTwitter.R
+++ extraction/extract_sentimentsTwitter.R
@@ -1,12 +1,15 @@
+# PTE: problema con le paquete "sentiment"
 # PTE: fallo de código en línea: "tm_map(tweets_collections, content_transformer(functio...."
+# PTE: añadir PositiveWordsResearch al prinicipio del sentiment analysis? (https://positivewordsresearch.com/list-of-positive-words/)
 # PTE: valorar problema de sólo 10 días hacia atrás (¿pagar? / ¿mantener predicciones corto plazo? / ¿usar otra fuente... news?)
 
 
-install.packages("ROAuth")
-install.packages("twitteR")
-install.packages("stringr")
-install.packages("tm")
-install.packages("wordcloud2")  # to draw tag clouds
+# install.packages("ROAuth")
+# install.packages("twitteR")
+# install.packages("stringr")
+# install.packages("tm")
+# install.packages("wordcloud2")  # to draw tag clouds
+# install.packages("sentimentAnalysis")  # to draw tag clouds
 
 library(tidyverse)
 library(ROAuth)
@@ -14,15 +17,20 @@ library(twitteR)
 library(stringr)
 library(tm)
 library(wordcloud2)
+library(sentiment)
+library(data.table)
+library(ggthemes)
+library(ggplot2)
+library(plotly)
 
-searchTerms <- c("Daimler", "@POTUS", "real oviedo")
+searchTerms <- c("Daimler", "@POTUS", "telecinco")
 
 # ================================
 # STEP 1: Extract words and frequency in twits
 # ================================
 
-i <- 3
-searchTerms[i]
+i <- 1
+searchTerms[1]
 
 # connect to twitter API from the twitter package
 setup_twitter_oauth(twitter_api_key, twitter_api_secret, twitter_access_token, twitter_access_token_secret)
@@ -66,7 +74,7 @@ term_matrix
 (frequency.terms <- findMostFreqTerms(term_matrix, lowfrequency = 20))
 # Transform it to matrix
 term_matrix <- as.matrix(term_matrix)
-term_matrix[1:10,1:25]
+term_matrix[1:10,1:20]
 # We compute the frequency of the distinct words from the tweets and we sort it by frequency
 distinctWordsfrequency <- sort(rowSums(term_matrix), decreasing = T)
 # Transform it to df
@@ -81,11 +89,14 @@ wordcloud2(distinctWordsDF)
 # ================================
 # STEP 2: Score Sentiment Function
 # ================================
-# We create the score sentiment function in order to run it afterwards in our daimler tweets Transform the vector with the sentences to simple array of scores with plyr package Then we clean the sentences with gsub() function Then we convert the letters from capital to lower case The below function was inspired by (https://medium.com/@rohitnair_94843/analysis-of-twitter-data-using-r-part-3-sentiment-analysis-53d0e5359cb8).
+# We create the score sentiment function in order to run it afterwards in our found tweets. Transform the vector with the sentences to simple array of scores with plyr package Then we clean the sentences with gsub() function Then we convert the letters from capital to lower case The below function was inspired by (https://medium.com/@rohitnair_94843/analysis-of-twitter-data-using-r-part-3-sentiment-analysis-53d0e5359cb8).
+
+# Obtain Positive and Negative words reference listings
 # We use the famous lexicon of posive and negative words that was created from : Liu, Bing, Minqing Hu, and Junsheng Cheng. “Opinion observer: analyzing and comparing opinions on the web.” In Proceedings of the 14th international conference on World Wide Web (WWW-2005), pp. 342-351. ACM, May 10-14, 2005. Thanks to Liu and Hu we will add more than 6500 positive phrases, idioms and quotes
 # https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html#lexicon
-
-# We will add some extra words, that were observed inside the review of tweets We merge the above two positive lexicons
+hu.liu.negative = scan("data/opinion-lexicon-English/negative-words.txt", what="character", comment.char=";")
+hu.liu.positive = scan("data/opinion-lexicon-English/positive-words.txt", what="character", comment.char=";")
+PositiveWordsResearch <- NULL
 pos.words <- c(hu.liu.positive, PositiveWordsResearch)
 # We add the extra words we noticed
 pos.words <- c(pos.words, "thanx", "awesome", "fantastic", "super", "prima", 
@@ -97,6 +108,68 @@ neg.words <- c(hu.liu.negative, "avoid", "lose", "loses", "scandal", "dieselgate
                "on hold", "cancel", "spam", "spams", "cancel", "wth", "Fight", "fighting", 
                "wtf", "arrest", "no", "not")
 
+
+
+score.sentiment = function(sentence, pos.words, neg.words, .progress = "none") {
+  scores = laply(sentences, function(sentence, pos.words, neg.words) {
+
+    sentence = gsub('https://','',sentence) # removes https://
+    sentence = gsub('http://','',sentence) # removes http://
+    sentence = gsub('[^[:graph:]]', ' ',sentence) ## removes graphic characters like emoticons
+    sentence = str_replace_all(sentence,"[^[:graph:]]", " ")
+    
+    sentence = gsub("[[:punct:]]", "", sentence) # removes punctuation 
+    sentence = gsub("[[:cntrl:]]", "", sentence) # removes control characters
+    sentence = gsub("\\d+", "", sentence) # removes numbers
+    sentence = tolower(sentence)
+    # With stringr package we distinct it into words
+    word.list = str_split(sentence, "\\s+")
+    # We unlist the vector
+    words = unlist(word.list)
+    # We compare the words from found tweets, with the above positive and
+    # negative words of the dictionaries
+    pos.matches = match(words, pos.words)
+    neg.matches = match(words, neg.words)
+    # The match function returned the position of the matched word otherwise NA
+    # Remove NAs
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    # We tranform the matches into 1 or 0 with the sum fiunction
+    score = sum(pos.matches) - sum(neg.matches)
+    return(score)
+  }, pos.words, neg.words, .progress = .progress)
+  scores.df = data.frame(score = scores, text = sentences)
+  return(scores.df)
+  
+}
+
+
+# We apply the above sentiment function on our tweets
+currentTerm.scores <- score.sentiment(tweets_unique, pos.words, neg.words, .progress = "none")
+currentTerm.scores
+sum(currentTerm.scores$score)
+
+
+# ================================
+# STEP 3: Sentiment Classification using Distant Supervision
+# ================================
+# We apply the “Sentiment Classification using Distant Supervision” to create the Sentiments this method was first applied by:Huang, Bhayani and Go Based on Huang, Bhayani and Go Twitter “Sentiment Classification using Distant Supervision” research paper for Stanford university published on Dec 18,2017 (https://www-cs.stanford.edu/people/alecmgo/papers/TwitterDistantSupervision09.pdf) They presented the results of machine learning algorithms for classifying the sentiment of Twitter messages using distant supervision. Their training data consists of Twitter messages with emoticons, which are used as noisy labels. This type of training data is abundantly available and can be obtained through automated means. We show that machine learning algorithms (Naive Bayes, Maximum Entropy, and SVM) have accuracy above 80% when trained with emoticon data.
+
+sentiments <- sentiment(tweets_unique)
+table(sentiments$polarity)
+sentiments$score <- 0
+sentiments$score[sentiments$polarity == "positive"] <- 1
+sentiments$score[sentiments$polarity == "negative"] <- -1
+
+sentiments$date <- as.IDate(tweets_found.nodups.df$Date)
+result <- aggregate(score ~ date, data = sentiments, mean)
+# We create an interactive plot with the  scoring results mean per date
+
+plotresults <- ggplot(result, aes(x = date, y = score)) + xlab("Date") + ylab("Sentiment Scoring Mean") + 
+  ggtitle("Sentiment Scoring Mean by Date") + theme_solarized(light = FALSE) + 
+  geom_path(color = "yellow", size = 1) + geom_point(color = "red", size = 3)
+
+ggplotly(plotresults)
 
 
 
