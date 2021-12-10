ee72a9d46e8547ad8f94fbbd0aa8edd1b1516fb8
JuanESuarez <prof02@yahoo.com>
2021-12-10 17:32:59 +0100

    upl

--- Z_test.R
+++ Z_test.R
@@ -1,18 +1,58 @@
-startDate <- "2015-12-31"
-yesterdayRefDate <- Sys.Date()-1
+# https://www.earthdatascience.org/courses/earth-analytics/get-data-using-apis/use-twitter-api-r/
+# https://cran.r-project.org/web/packages/rtweet/vignettes/intro.html
 
-unProcessedDates <- NULL
-allPossibleDates <- seq(ymd(startDate, tz = "UTC"), ymd(yesterdayRefDate, tz = "UTC"), by="days")
-allPossibleDates
+# install.packages("rtweet")
+
+# load twitter library - the rtweet library is recommended now over twitteR
+library(rtweet)
+# plotting and pipes - tidyverse!
+library(ggplot2)
+library(dplyr)
+# text mining library
+library(tidytext)
+
+# create token named "twitter_token"
+twitter_token <- create_token(
+  app = twitter_api_appname,
+  consumer_key = twitter_api_key,
+  consumer_secret = twitter_api_secret,
+  access_token = twitter_access_token,
+  access_secret = twitter_access_token_secret)
 
 
-dateStart <- allPossibleDates[1]
-dateEnd <- dateStart + hours(23) + minutes(59) + seconds(59)  
-dateEnd_sample <- dateStart + hours(02) + minutes(00) + seconds(00)  # API restricted to 2 hours for total flights
 
-dateStart
-dateEnd 
-dateEnd_sample
 
-length(getIntervalFlights(startTime=dateStart, endTime=dateEnd_sample, timeZone="UTC"))
+## search for 500 tweets using the #rstats hashtag
+rstats_tweets <- search_tweets(q = "#rstats", n = 500)
+names(rstats_tweets)
+rstats_tweets$screen_name
+unique(as.Date(rstats_tweets$created_at))
+rstats_tweets$text
 
+
+
+
+## stream london tweets for a week (60 secs x 60 mins * 24 hours *  7 days)
+stream_tweets(
+  "realdonaldtrump,trump",
+  timeout = 60 * 60 * 24 * 7,
+  file_name = "tweetsabouttrump.json",
+  parse = FALSE
+)
+## read in the data as a tidy tbl data frame
+djt <- parse_stream("tweetsabouttrump.json")
+
+
+
+## lookup users by screen_name or user_id
+users <- c("KimKardashian", "justinbieber", "taylorswift13",
+           "espn", "JoelEmbiid", "cstonehoops", "KUHoops",
+           "upshotnyt", "fivethirtyeight", "hadleywickham",
+           "cnn", "foxnews", "msnbc", "maddow", "seanhannity",
+           "potus", "epa", "hillaryclinton", "realdonaldtrump",
+           "natesilver538", "ezraklein", "annecoulter")
+famous_tweeters <- lookup_users(users)
+## preview users data
+famous_tweeters
+# extract most recent tweets data from the famous tweeters
+tweets_data(famous_tweeters)
--- extraction/extract_sentimentsTwitter.R
+++ extraction/extract_sentimentsTwitter.R
@@ -1,50 +1,114 @@
-# PTE: revisar filtros ¿por qué no hay nada de @POTUS?
+# PTE: conflicto entre ply y dplyr
 # PTE: lista asociada de sinónimos de cada término
+# PTE: distinguir entre términos from: y about:
 # PTE: añadir PositiveWordsResearch al prinicipio del sentiment analysis? (https://positivewordsresearch.com/list-of-positive-words/)
-# PTE: sacar inicialización de las listas de palabras del loop
 # PTE: fechas: valorar con Twitter problema de sólo 10 días hacia atrás (¿pagar? / ¿mantener predicciones corto plazo? / ¿usar otra fuente... news?) COMPROBAR NIVEL ACTUAL EN TWITTER ("Elevated?"): https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api#Access
 
 
 # https://www.rpubs.com/JaimeFC/415349
 # https://rstudio-pubs-static.s3.amazonaws.com/276096_5f10707713d7468fb5a3834a85c2de72.html
 
-searchTerms <- c("Daimler", "@POTUS", "telecinco")
+# https://cran.r-project.org/web/packages/rtweet/vignettes/intro.html
 
-
+searchTerms <- c("Daimler", "@POTUS", "Amazon")
 
 library(tidyverse)
+library(plyr)
 library(dplyr)
 library(ROAuth)
 library(twitteR)
 library(stringr)
-library(data.table)
+
+
+# connect to twitter API from the twitter package
+setup_twitter_oauth(twitter_api_key, twitter_api_secret, twitter_access_token, twitter_access_token_secret)
+
 
 # ================================
-# STEP 1: Extract texts from Twitter
+# STEP 1: Obtain Positive and Negative words reference listings
+# ================================  
+# We use the famous lexicon of posive and negative words that was created from : Liu, Bing, Minqing Hu, and Junsheng Cheng. “Opinion observer: analyzing and comparing opinions on the web.” In Proceedings of the 14th international conference on World Wide Web (WWW-2005), pp. 342-351. ACM, May 10-14, 2005. Thanks to Liu and Hu we will add more than 6500 positive phrases, idioms and quotes
+# https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html#lexicon
+hu.liu.negative = scan("data/opinion-lexicon-English/negative-words.txt", what="character", comment.char=";")
+hu.liu.positive = scan("data/opinion-lexicon-English/positive-words.txt", what="character", comment.char=";")
+PositiveWordsResearch <- NULL
+# We add the extra words we noticed
+pos.words <- c(hu.liu.positive, PositiveWordsResearch)
+pos.words <- c(pos.words, "thanx", "awesome", "fantastic", "super", "prima", 
+               "toll", "cool", "geil", "profit", "profits", "earnings", "congrats", "prizes", 
+               "prize", "thanks", "thnx", "Grt", "gr8", "plz", "trending", "recovering", 
+               "brainstorm", "leader")
+neg.words <- c(hu.liu.negative, "avoid", "lose", "loses", "scandal", "dieselgate", 
+               "sucks", "awful", "disgusting", "negative", "wait", "waiting", "hold", "onhold", 
+               "on hold", "cancel", "spam", "spams", "cancel", "wth", "Fight", "fighting", 
+               "wtf", "arrest", "no", "not")
+
+
+# ================================
+# STEP 2: Sentiment function
+# ================================
+# We create the score sentiment function in order to run it afterwards in our found tweets
+
+score.sentiment = function(sentences, pos.words, neg.words, .progress = 'none') {
+  scores = laply(sentences, function(sentence, pos.words, neg.words) {
+    sentence = gsub('https://','',sentence) # removes https://
+    sentence = gsub('http://','',sentence) # removes http://
+    sentence = gsub('[^[:graph:]]', ' ',sentence) ## removes graphic characters like emoticons
+    sentence = str_replace_all(sentence,"[^[:graph:]]", " ")
+    
+    sentence = gsub("[[:punct:]]", "", sentence) # removes punctuation (and @)
+    sentence = gsub("[[:cntrl:]]", "", sentence) # removes control characters
+    sentence = gsub("\\d+", "", sentence) # removes numbers
+    sentence = tolower(sentence)
+    # With stringr package we distinct it into words
+    word.list = str_split(sentence, "\\s+")
+    # We unlist the vector
+    words = unlist(word.list)
+    # We compare the words from found tweets, with the above positive and
+    # negative words of the dictionaries
+    pos.matches = match(words, pos.words)
+    neg.matches = match(words, neg.words)
+    # The match function returned the position of the matched word otherwise NA
+    # Remove NAs
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    # We tranform the matches into 1 or 0 with the sum fiunction
+    score = sum(pos.matches) - sum(neg.matches)
+    return(score)
+  }, pos.words, neg.words, .progress=.progress)
+  scores.df = data.frame(score = scores, text = sentences)
+  return(scores.df)
+}
+
+
+
+# ================================
+# STEP 3: Extract texts from Twitter
 # ================================
 
 # >>>>>
 # >>>>>
 scores.df <- data.frame()
+
 for (i in c(1:length(searchTerms))) {
-  
-  print(searchTerms[i])
-  
-  # connect to twitter API from the twitter package
-  setup_twitter_oauth(twitter_api_key, twitter_api_secret, twitter_access_token, twitter_access_token_secret)
   print(paste("Reading tweets:",searchTerms[i]))
+
   tweets_found <- 
-    searchTwitter(searchTerms[i], n = 1000, resultType = "recent", lang = "en", retryOnRateLimit = 100)
+    searchTwitter(searchTerms[i], n = 50, resultType = "popular", lang = "en", retryOnRateLimit = 100)
   # convert the tweets_found from list to a data frame
   tweets_found.df <- twListToDF(tweets_found)
   print(paste("Tweets found:",nrow(tweets_found.df)))
   head(tweets_found.df)
+  tweets_found.df$text
+  tweets_found.nodups.df$text
+  
   # remove repeated and clean "..."
   tweets_found.nodups.df <- distinct(tweets_found.df, text, .keep_all = TRUE)
   tweets_found.nodups.df$text <- gsub("… ", "", tweets_found.nodups.df$text)
   # change the feature created to Date
   tweets_found.nodups.df <- tweets_found.nodups.df %>% 
-    mutate(created = as.Date(tweets_found.nodups.df$created)) %>% rename(date = created)
+    mutate(created = as.Date(tweets_found.nodups.df$created)) 
+  names(tweets_found.nodups.df)[names(tweets_found.nodups.df) == "created"] <- "date"
   # transform from datetime to date format
   tweets_text <- lapply(tweets_found, function(x) x$getText())
   # delete the (retweets)
@@ -52,59 +116,6 @@ for (i in c(1:length(searchTerms))) {
   # obtain cleaned tweets and their dates
   length(tweets_found.nodups.df$date)
   length(tweets_unique)
-  
-  # ================================
-  # STEP 2: Score Sentiment Function
-  # ================================
-  # We create the score sentiment function in order to run it afterwards in our found tweets.
-  # Obtain Positive and Negative words reference listings
-  # We use the famous lexicon of posive and negative words that was created from : Liu, Bing, Minqing Hu, and Junsheng Cheng. “Opinion observer: analyzing and comparing opinions on the web.” In Proceedings of the 14th international conference on World Wide Web (WWW-2005), pp. 342-351. ACM, May 10-14, 2005. Thanks to Liu and Hu we will add more than 6500 positive phrases, idioms and quotes
-  # https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html#lexicon
-  
-  
-  hu.liu.negative = scan("data/opinion-lexicon-English/negative-words.txt", what="character", comment.char=";")
-  hu.liu.positive = scan("data/opinion-lexicon-English/positive-words.txt", what="character", comment.char=";")
-  PositiveWordsResearch <- NULL
-  # We add the extra words we noticed
-  pos.words <- c(hu.liu.positive, PositiveWordsResearch)
-  pos.words <- c(pos.words, "thanx", "awesome", "fantastic", "super", "prima", 
-                 "toll", "cool", "geil", "profit", "profits", "earnings", "congrats", "prizes", 
-                 "prize", "thanks", "thnx", "Grt", "gr8", "plz", "trending", "recovering", 
-                 "brainstorm", "leader")
-  neg.words <- c(hu.liu.negative, "avoid", "lose", "loses", "scandal", "dieselgate", 
-                 "sucks", "awful", "disgusting", "negative", "wait", "waiting", "hold", "onhold", 
-                 "on hold", "cancel", "spam", "spams", "cancel", "wth", "Fight", "fighting", 
-                 "wtf", "arrest", "no", "not")
-  score.sentiment = function(sentences, pos.words, neg.words, .progress = 'none') {
-    scores = laply(sentences, function(sentence, pos.words, neg.words) {
-      sentence = gsub('https://','',sentence) # removes https://
-      sentence = gsub('http://','',sentence) # removes http://
-      sentence = gsub('[^[:graph:]]', ' ',sentence) ## removes graphic characters like emoticons
-      sentence = str_replace_all(sentence,"[^[:graph:]]", " ")
-      
-      sentence = gsub("[[:punct:]]", "", sentence) # removes punctuation 
-      sentence = gsub("[[:cntrl:]]", "", sentence) # removes control characters
-      sentence = gsub("\\d+", "", sentence) # removes numbers
-      sentence = tolower(sentence)
-      # With stringr package we distinct it into words
-      word.list = str_split(sentence, "\\s+")
-      # We unlist the vector
-      words = unlist(word.list)
-      # We compare the words from found tweets, with the above positive and
-      # negative words of the dictionaries
-      pos.matches = match(words, pos.words)
-      neg.matches = match(words, neg.words)
-      # The match function returned the position of the matched word otherwise NA
-      # Remove NAs
-      pos.matches = !is.na(pos.matches)
-      neg.matches = !is.na(neg.matches)
-      # We tranform the matches into 1 or 0 with the sum fiunction
-      score = sum(pos.matches) - sum(neg.matches)
-      return(score)
-    }, pos.words, neg.words, .progress=.progress)
-    scores.df = data.frame(score = scores, text = sentences)
-    return(scores.df)
-  }
   
   # We apply the above sentiment function on our tweets
   currentTerm.scores <- score.sentiment(tweets_unique, pos.words, neg.words, .progress = "none")
@@ -121,10 +132,19 @@ for (i in c(1:length(searchTerms))) {
 }
 # <<<<<
 # <<<<<
+
+# if("dplyr" %in% (.packages())){
+#   detach("package:dplyr", unload=TRUE) 
+#   detach("package:plyr", unload=TRUE) 
+# } 
+# library(plyr)
+# library(dplyr)
+
 scores.df <- scores.df %>% 
   group_by(date, term) %>% 
   summarise(score=mean(score))
 head(scores.df)
 
-scores.df <- scores.df %>% spread(key = term, value = score, fill=NA)
-head(scores.df)
+scores.df_ts <- scores.df %>% 
+  spread(key = term, value = score, fill=NA)
+head(scores.df_ts)
