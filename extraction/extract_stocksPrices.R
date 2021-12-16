22c30be47f212f5eaaf0e86f5d1e5a57d44b7ccf
JuanESuarez <prof02@yahoo.com>
2021-12-16 22:05:46 +0100

    upl

--- extraction/extract_stocksPrices.R
+++ extraction/extract_stocksPrices.R
@@ -1,5 +1,3 @@
-# PTE: revisar el warning en TIDYQUANT
-# PTE: probar paquete QUANTMOD + ver si vale para "^VIX" ó "EURUSD=X" 
 # PTE: prueba de volumen de symbols
 
 
@@ -14,66 +12,6 @@ print("Extracting stock prices and volumes")
 
 endDateTicker <- today()
 startingDateTicker <- "1900-01-01"
-
-
-
-
-
-
-
-
-# ~~~~~~~~~~~~~~
-# PROBLEMA DE LOS WARNINGS
-# https://www.giters.com/business-science/tidyquant/issues/202
-tq_get(c('^VIX'), 
-       from = '2021-12-14', 
-       to = '2021-12-16', 
-       get = 'stock.prices')
-
-
-
-# PACKAGE QUANTMOD
-# https://bookdown.org/kochiuyu/Technical-Analysis-with-R/downloading-data.html
-# https://cran.r-project.org/web/packages/quantmod/quantmod.pdf
-test <- getSymbols("GOLD;SPY", from="1997-12-31", src='yahoo') 
-head(test)
-getSymbols("EURUSD=")
-barChart(EURUSD=)
-
-install.packages("quantmod")
-library(quantmod)
-getSymbols("GOLD")
-getSymbols("AAPL", from='1980-01-01',to='2021-12-16')
-getSymbols(AAPL)
-
-
-tickers <- c("^IBEX", "NDAQ", "EURUSD=X", "GOLD", "^VIX", "AAPL", "NFLX", "AMZN", "AAIC", "MSFT", "AAN")
-tickers <- c('AXP','CAT')
-getSymbols(tickers, from = '2018-03-01')
-dfNew <- data.frame(Company = character(),
-                    Date=as.Date(character()),
-                    Close=numeric(),
-                    Volume=numeric(), 
-                    stringsAsFactors=FALSE) 
-for(i in 1:length(tickers)){
-  dfSym <- cbind(Company=rep(tickers[i],nrow(get(tickers[i]))),fortify.zoo(get(tickers[i])[,5]))
-  names(dfSym)[2:3] <- c('Date','Volume')
-  dfNew <- rbind(dfNew,dfSym)
-}
-head(dfNew)
-unique(dfNew$Company)
-
-# ~~~~~~~~~~~~~~
-
-
-
-
-
-
-
-
-
-
 
 stocksData <- tq_get(chosenTickers,
                  from = startingDateTicker,
