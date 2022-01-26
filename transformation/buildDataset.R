fd43b473c3df8ef2c37f5d7679bc04f9fa906545
SUAREZ <Juan-Eloy.Suarez@dupont.com>
2022-01-26 12:58:13 +0100

    upl

--- transformation/buildDataset.R
+++ transformation/buildDataset.R
@@ -1,17 +1,46 @@
-# PTE: Create network (https://www.jessesadler.com/post/network-analysis-with-r/)
-
-# -----------------------------------------------------------------
-# -----------------------------------------------------------------
-# 
-# -----------------------------------------------------------------
-# -----------------------------------------------------------------
+# Fusionar con "initialization/load_extractionScope.R"
 
 library(tidyverse)
-library(openxlsx)
 
 
 # ------------------------------------------------------
-# Extract Features and Geography Catalogs
+# Extract seed (features and locations to use as hypothesis)
+# ------------------------------------------------------
+
+seed1 <- read.xlsx("docs/seed1.xlsx")
+head(seed1)
+names(seed1)
+
+goalFeatures <- seed1$FeatureCode
+goalFeatureNames <- seed1$FeatureName
+moodFeatures <- strsplit(seed1$PlanetMoodFeatures[1], "\n")[[1]]
+specificSearchConcepts <- seed1$SpecificSearchConcepts[!(is.na(seed1$SpecificSearchConcepts))]
+genericSearchConcepts <- strsplit(seed1$GenericSearchConcepts[1], "\n")[[1]]
+searchConcepts <- c(specificSearchConcepts, genericSearchConcepts)
+specificSentimentConcepts <- seed1$SpecificSentimentConcepts[!(is.na(seed1$SpecificSentimentConcepts))]
+genericSentimentConcepts <- strsplit(seed1$GenericSentimentConcepts[1], "\n")[[1]]
+sentimentConcepts <- c(specificSentimentConcepts, genericSentimentConcepts)
+specificStd_Geo <- seed1$SpecificStd_Geo[!(is.na(seed1$SpecificStd_Geo))]
+genericStd_Geo <- strsplit(seed1$GenericStd_Geo[1], "\n")[[1]]
+geoLocations <- c(specificStd_Geo, genericStd_Geo)
+
+goalFeatures
+moodFeatures
+searchConcepts
+sentimentConcepts
+geoLocations
+
+
+# ------------------------------------------------------
+# Load standard geography (previously validated by user)
+# ------------------------------------------------------
+
+std_geo <- read.xlsx("userEdition/standardGeography.xlsx")
+head(std_geo)
+
+
+# ------------------------------------------------------
+# Get data from extracted .RDS files
 # ------------------------------------------------------
 
 airTraffic <- readRDS("data/data_airTraffic_ts.rds")
@@ -21,7 +50,6 @@ music <- readRDS("data/data_music_ts.rds")
 OECD <- readRDS("data/data_OECD_ts.rds")
 searchesGoogle <- readRDS("data/data_searchesGoogle_ts.rds")
 twitterSentiment <- readRDS("data/data_twitterSentiment_ts.rds")
-weather <- readRDS("data/data_weather_ts.rds")
 stocks <- readRDS("data/data_stocks_ts.rds")
 
 head(airTraffic)
@@ -31,48 +59,15 @@ head(music)
 head(OECD)
 head(searchesGoogle)
 head(twitterSentiment)
-head(weather)
 head(stocks)
 
-airTraffic_features <- paste0("airTraffic.", airTraffic %>% select (-c(date, countryCode)) %>% names())
-FIFA_features <- paste0("airTraffic.", FIFA %>% select (-c(Date)) %>% names())
-moonSun_features <- paste0("moonSun.", moonSun %>% select (-c(date, countryCode)) %>% names())
-music_features <- paste0("music.", music %>% select (-c(date, country, countryCode)) %>% names())
-OECD_features <- paste0("OECD.", OECD %>% select (-c(Date, Country)) %>% names())
-searchesGoogle_features <- paste0("searchesGoogle.", searchesGoogle %>% select (-c(date)) %>% names())
-twitterSentiment_features <- paste0("twitterSentiment.", twitterSentiment %>% select (-c(date)) %>% names())
-weather_features <- paste0("weather.", weather %>% select (-c(date, stationPlace)) %>% names())
-stocks_features <- paste0("stocks.", stocksData %>% select (-c(date)) %>% names())
-
-all_features <- c(
-  airTraffic_features, 
-  FIFA_features, 
-  moonSun_features, 
-  music_features, 
-  OECD_features, 
-  searchesGoogle_features, 
-  twitterSentiment_features, 
-  weather_features, 
-  stocks_features)
-
-all_features
-
-
 
 # ------------------------------------------------------
-# Extract Features and Geography Catalogs
+# Build a dataset based specifically in the seed (hypothesis)
 # ------------------------------------------------------
 
-std_geo_revised <- read.xlsx("userEdition/standardGeography.xlsx")
-std_geo_list <- unique(std_geo_revised$stdCountryName)
 
-# ------------------------------------------------------
-# Print obtained catalogs
-# ------------------------------------------------------
 
-print("AVAILABLE FEATURES:")
-print(all_features)
-print("AVAILABLE GEOGRAPHY DIMENSIONS:")
-head(std_geo_revised)
-print(std_geo_list)
+
+
 
