2d569d0ecb50724a15c1d3ce4b0f42f4267e92a6
SUAREZ <Juan-Eloy.Suarez@dupont.com>
2022-01-28 17:08:17 +0100

    upl

--- transformation/buildDataset.R
+++ transformation/buildDataset.R
@@ -5,18 +5,6 @@ library(tidyverse)
 library(openxlsx)
 
 # ------------------------------------------------------
-# Extract seed (features and locations to use as hypothesis)
-# ------------------------------------------------------
-allFeatures_df <- readRDS("data/featuresSeed.rds")
-head(allFeatures_df)
-# ------------------------------------------------------
-# Load standard geography (previously validated by user)
-# ------------------------------------------------------
-std_geo <- read.xlsx("userEdition/standardGeography.xlsx")
-head(std_geo)
-
-
-# ------------------------------------------------------
 # Get data from extracted .RDS files
 # ------------------------------------------------------
 airTraffic <- readRDS("data/data_airTraffic_ts.rds")
@@ -27,7 +15,6 @@ OECD <- readRDS("data/data_OECD_ts.rds")
 searchesGoogle <- readRDS("data/data_searchesGoogle_ts.rds")
 twitterSentiment <- readRDS("data/data_twitterSentiment_ts.rds")
 stocks <- readRDS("data/data_stocks_ts.rds")
-
 
 # ------------------------------------------------------
 # Convert locations align field names, add geo field if not present
@@ -40,63 +27,62 @@ music <- music %>%
   mutate(countryCode = toupper(countryCode)) %>% 
   left_join((reduced_std_geo %>% filter(source=="music")), by = "countryCode") %>% 
   select(-c("countryCode", "country"))
+names(music) <- paste0("music.", names(music))
+music <- music %>% rename(date = music.date)
+music <- music %>% rename(stdCountryCode = music.stdCountryCode)
 
 FIFA <- FIFA %>% 
   mutate(CountryCode = toupper(CountryCode)) %>% 
   left_join((reduced_std_geo %>% filter(source=="FIFA")), by = c("CountryCode" = "countryCode")) %>% 
   rename("date" = "Date") %>% 
   select(-c("CountryCode", "source"))
-  
+names(FIFA) <- paste0("FIFA.", names(FIFA))
+FIFA <- FIFA %>% rename(date = FIFA.date)
+FIFA <- FIFA %>% rename(stdCountryCode = FIFA.stdCountryCode)
+
 moonSun <- moonSun %>% 
   mutate(countryCode = toupper(countryCode)) %>% 
   left_join((reduced_std_geo %>% filter(source=="moonSun")), by = "countryCode") %>% 
   select(-c("countryCode", "source"))
 moonSun %>% filter(date =="1960-01-01")
+names(moonSun) <- paste0("moonSun.", names(moonSun))
+moonSun <- moonSun %>% rename(date = moonSun.date)
+moonSun <- moonSun %>% rename(stdCountryCode = moonSun.stdCountryCode)
 
 OECD <- OECD %>% 
   mutate(countryCode = toupper(Country)) %>% 
   left_join((reduced_std_geo %>% filter(source=="OECD")), by = c("Country" = "countryCode")) %>% 
   rename("date" = "Date") %>% 
   select(-c("countryCode", "Country", "source"))
-
+names(OECD) <- paste0("OECD.", names(OECD))
+OECD <- OECD %>% rename(date = OECD.date)
+OECD <- OECD %>% rename(stdCountryCode = OECD.stdCountryCode)
 
 # Add geo field (as NA) if not present, to facilitate merge
 airTraffic <- airTraffic %>% mutate(stdCountryCode = NA)
-searchesGoogle <- searchesGoogle %>% mutate(stdCountryCode = NA)
-twitterSentiment <- twitterSentiment %>% mutate(stdCountryCode = NA)
-stocks <- stocks %>% mutate(stdCountryCode = NA)
+names(airTraffic) <- paste0("airTraffic.", names(airTraffic))
+airTraffic <- airTraffic %>% rename(date = airTraffic.date)
+airTraffic <- airTraffic %>% rename(stdCountryCode = airTraffic.stdCountryCode)
 
+searchesGoogle <- searchesGoogle %>% mutate(stdCountryCode = NA)
+names(searchesGoogle) <- paste0("searchesGoogle.", names(searchesGoogle))
+searchesGoogle <- searchesGoogle %>% rename(date = searchesGoogle.date)
+searchesGoogle <- searchesGoogle %>% rename(stdCountryCode = searchesGoogle.stdCountryCode)
+
+twitterSentiment <- twitterSentiment %>% mutate(stdCountryCode = NA)
+names(twitterSentiment) <- paste0("twitterSentiment.", names(twitterSentiment))
+twitterSentiment <- twitterSentiment %>% rename(date = twitterSentiment.date)
+twitterSentiment <- twitterSentiment %>% rename(stdCountryCode = twitterSentiment.stdCountryCode)
+
+stocks <- stocks %>% mutate(stdCountryCode = NA)
+names(stocks) <- paste0("stocks.", names(stocks))
+stocks <- stocks %>% rename(date = stocks.date)
+stocks <- stocks %>% rename(stdCountryCode = stocks.stdCountryCode)
 
 
 # ------------------------------------------------------
 # Merge a dataset based specifically in the seed (hypothesis)
 # ------------------------------------------------------
-
-
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# test1 <- data.frame(
-#   date=c("2021/12/20", "2021/12/20", "2021/12/19", "2021/12/19", "2021/12/18", "2021/12/18", "2021/12/17", "2021/12/17"),
-#   vbleJ=c(2300, 4800, 5500, 8300, 3900, 700, 900, 1300), 
-#   vbleK=c(12300, 14800, 15500, 18300, 13900, 1700, 9100, 12800), 
-#   vbleL=c(3, 3, 3, 5, 6, 8, 5, 7),
-#   stdCountryCode=c("ESP", "US", "ESP", "US", "FR", "US", "ESP", "FR"))
-# test2 <- data.frame(
-#   date=c("2021/12/20", "2021/12/20", "2021/12/19", "2021/12/19", "2021/12/18", "2021/12/18"),
-#   vbleA=c(23, 48, 55, 83, 39, 7), 
-#   vbleB=c(123, 148, 155, 183, 139, 17),
-#   stdCountryCode=c("ESP", NA, NA, NA, NA, "FR"))
-# test1
-# test2
-# 
-# # Outer Join: 
-# merge(test1, test2, by = c("date", "stdCountryCode"), all=TRUE)
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
 
 head(airTraffic)
 head(FIFA)
@@ -119,6 +105,22 @@ fullDataset_raw <- merge(fullDataset_raw, stocks, by = c("date", "stdCountryCode
 
 
 # ------------------------------------------------------
+# Keep only data related to defined geo-filters
+# ------------------------------------------------------
+# Load standard geography (previously validated by user)
+std_geo <- read.xlsx("userEdition/standardGeography.xlsx")
+head(std_geo)
+
+# convert country names from seed in standard codes
+geoCodesSeed <- allFeatures_df %>% 
+  filter(source=="locations") %>% select(variable) %>% 
+  left_join(std_geo, by = c("variable" = "countryName")) %>% pull(stdCountryCode)
+
+# filter only geolocations specidfied in the seed
+fullDataset_raw <- fullDataset_raw %>% filter(stdCountryCode %in% c(NA, geoCodesSeed))
+
+
+# ------------------------------------------------------
 # Save consolidated raw dataset, still to refine
 # ------------------------------------------------------
 saveRDS(fullDataset_raw,"data/dataset_raw.rds")
@@ -131,18 +133,29 @@ saveRDS(fullDataset_raw,"data/dataset_raw.rds")
 
 
 # ------------------------------------------------------
-# Load consolidated raw dataset, still to refine
+# Remove not applying features 
 # ------------------------------------------------------
+# Load consolidated raw dataset, still to refine
 fullDataset_raw <- readRDS("data/dataset_raw.rds")
 head(fullDataset_raw)
 
 
-fullDataset_raw %>% filter(is.na(stdCountryCode)) %>% arrange(desc(date))
-fullDataset_raw %>% filter(!is.na(stdCountryCode)) %>% arrange(desc(date))
+
+# Extract seed (features and locations to use as hypothesis)
+allFeatures_df <- readRDS("data/featuresSeed.rds")
+head(allFeatures_df)
+# Extract header names of the seed features
+seedVbles <- allFeatures_df %>% 
+  filter(source != "locations") %>% 
+  mutate(vbleName = paste0(source, ".", variable)) %>% 
+  pull(vbleName)
+
+seedVbles
+names(fullDataset_raw)
 
 
-# ------------------------------------------------------
-# Remove not applying features and keep only defined geo-filters
-# ------------------------------------------------------
+
+
+
 
 
