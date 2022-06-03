6046ceb39c9812b7e95ea53a5f0f810bccadada1
SUAREZ <Juan-Eloy.Suarez@dupont.com>
2022-06-03 13:24:13 +0200

    upl

--- test_VAR.R
+++ test_VAR.R
@@ -22,171 +22,17 @@ select <- dplyr::select # vars uses package MASS, which "select" function causes
 library(tsDyn) # VECM
 
 
+
 #========================================================
 # Data
 #========================================================
 
 # forecasting horizon
 nhor <- 12
-
-#——————————————-
-# quarterly data related with money demand
-#——————————————-
-# LRM : logarithm of real money M2 (LRM)
-# LRY : logarithm of real income (LRY)
-# LPY : logarithm of price deflator (LPY)
-# IBO : bond rate (IBO)
-# IDE : bank deposit rate (IDE)
-# the period 1974:Q1 - 1987:Q3
-#——————————————-
-
-# selected variables
-data(denmark)
-df.lev <- denmark[,c('LRM','LRY','IBO','IDE')]
-m.lev  <- as.matrix(df.lev)
-nr_lev <- nrow(df.lev)
-
-# quarterly centered dummy variables
-dum_season <- data.frame(yyyymm = denmark$ENTRY)
-substr.q   <- as.numeric(substring(denmark$ENTRY, 6,7))
-dum_season$Q2 <- (substr.q==2)-1/4
-dum_season$Q3 <- (substr.q==3)-1/4
-dum_season$Q4 <- (substr.q==4)-1/4
-dum_season    <- dum_season[,-1]
-
-# Draw Graph
-str.main <- c(
-  'LRM=ln(real money M2)', 'LRY=ln(real income)', 
-  'IBO=bond rate', 'IDE=bank deposit rate')
-x11(width=12, height = 6); 
-par(mfrow=c(2,2), mar=c(5,3,3,3))
-for(i in 1:4) {
-  matplot(m.lev[,i], axes=FALSE,
-          type=c('l'), col = c('blue'), 
-          main = str.main[i])
-  axis(2) # show y axis
-  # show x axis and replace it with 
-  # an user defined sting vector
-  axis(1, at=seq_along(1:nrow(df.lev)),
-       labels=denmark$ENTRY, las=2)
-}
-
-#========================================================
-# VAR model in level
-#========================================================
-
-# lag length p
-# Lag length (p) is selected by using several information criteria : AIC, HQ, SC, and so on. Lower these scores are better since these criteria penalize models that use more parameters. This work can be done easily by using VARselect() function with a maximum lag. From the following results, we set lag lengths of VAR in level and VAR in difference to 2 and 1 respectively.
-VARselect(df.lev, lag.max = 4, type = 'const', season = 4)
-
-# estimation
-var.model_lev <- VAR(df.lev, p = 2, type = 'const', season = 4)
-
-# forecast of lev data
-var.pred <- predict(var.model_lev, n.ahead = nhor)
-x11(); par(mai=rep(0.4, 4)); plot(var.pred)
-x11(); par(mai=rep(0.4, 4)); fanchart(var.pred)
-
-
-#========================================================
-# VAR model in difference using vars
-#========================================================
-
-# 1st differenced data
-df.diff <- diff(as.matrix(df.lev), lag = 1)
-colnames(df.diff) <- c('dLRM', 'dLRY', 'dIBO', 'dIDE')
-m.diff <- as.matrix(df.diff)
-
-# lag length
-VARselect(df.diff, lag.max = 4, type = 'const', season = 4)
-
-# estimation
-vare_diff <- VAR(df.diff, p = 1, type = 'const', season = 4)
-
-# forecast of differenced data
-varf_diff <- predict(vare_diff, n.ahead = nhor)
-x11(); par(mai=rep(0.4,4)); plot(varf_diff)
-x11(); par(mai=rep(0.4,4)); fanchart(varf_diff)
-
-# recover lev forecast
-m.varf_lev_ft <- rbind(m.lev, matrix(NA, nhor, 4))
-m.ft_df <- do.call(cbind,lapply(varf_diff$fcst, 
-                                function(x) x[,'fcst']))
-
-# growth to level
-for(h in (nr_lev+1):(nr_lev+nhor)) {
-  hf <- h - nr_lev
-  m.varf_lev_ft[h,] <- m.varf_lev_ft[h-1,] + m.ft_df[hf,]
-}
-
-# Draw Graph
-x11(width=8, height = 8); 
-par(mfrow=c(4,1), mar=c(2,2,2,2))
-
-for(i in 1:4) {
-  df <- m.varf_lev_ft[,i]
-  matplot(df, type=c('l'), col = c('blue'), 
-          main = str.main[i]) 
-  abline(v=nr_lev, col='blue')
-}
-
-
-#========================================================
-# VAR model in difference using tsDyn
-#========================================================
-
-linevare_diff <- lineVar(data = df.lev, lag = 1, include = 'const',
-                         model = 'VAR', I = 'diff', beta = NULL, exogen = dum_season)
-
-# check if both models (vars & tsDyn) yield same coefficients
-linevare_diff 
-do.call(rbind,lapply(vare_diff$varresult, 
-                     function(x) x$coefficients))
-
-# quarterly centered dummy variables for forecast
-dumf_season <- rbind(tail(dum_season,4),
-                     tail(dum_season,4),
-                     tail(dum_season,4))
-# forecast
-linevarf_diff <- predict(linevare_diff, n.ahead = nhor, 
-                         exoPred = dumf_season) 
-# Draw Graph
-x11(width=8, height = 8); 
-par(mfrow=c(4,1), mar=c(2,2,2,2))
-
-df <- rbind(df.lev, linevarf_diff)
-
-for(i in 1:4) {
-  matplot(df[,i], type=c('l'), col = c('blue'), 
-          main = str.main[i]) 
-  abline(v=nr_lev, col='blue')
-}
-
-
-
-
-
-
-
-
-
-
-
-# =================================================================
-# =================================================================
 
 df_planetMood <- readRDS("data/df_planetMood.rds")  # Dataset ready for analysis 
-
 names(df_planetMood)
 head(as_tibble(df_planetMood))
-
-
-#========================================================
-# Data
-#========================================================
-
-# forecasting horizon
-nhor <- 12
 
 # selected variables
 df.lev <- df_planetMood[,c('stocks.^VIX_GLOBAL','stocks.^VVIX_GLOBAL','music.tempo_GLOBAL','GDELT.tone_GLOBAL')]
