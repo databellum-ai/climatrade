2fd27676ea2b61ac540f5ca0afd4754eb44cc1fe
SUAREZ <Juan-Eloy.Suarez@dupont.com>
2022-06-12 19:19:34 +0200

    upl

--- extraction/extract_stocksPrices.R
+++ extraction/extract_stocksPrices.R
@@ -16,17 +16,32 @@ print("Extracting stocks")
 
 endDateTicker <- today()
 startingDateTicker <- "1900-01-01"
-stocksData <- tq_get(chosenTickers,
-                 from = startingDateTicker,
-                 to = endDateTicker,
-                 get = "stock.prices")
 
-# we extract close price
-stocksData <- stocksData %>% select(date, symbol, value = close) %>% group_by(date, symbol) %>% summarise(value = mean(value)) %>% arrange(desc(date))
 
-stocksData_ts <- stocksData %>% 
-  spread(key=symbol, value = value) %>% mutate(countryCode = NA) %>% 
+
+# !! CALCULAR MA, HL Y V... STOCK A STOCK!
+stocksData2 <- 
+  tq_get(chosenTickers, from = startingDateTicker, to = endDateTicker, get = "stock.prices") %>% 
+  mutate(close = na_locf(close)) %>% 
+  tq_mutate(select = close, mutate_fun = EMA, n = 50, col_rename = "MA") %>% 
+  tq_mutate(select = close, mutate_fun = volatility, calc="close", col_rename = "V") %>% 
+  mutate(HL = (high - low)) %>% 
+  select(date, symbol, close, MA, HL, V) %>% 
   arrange(desc(date))
+
+# stocksData <- tq_get(chosenTickers,
+#                  from = startingDateTicker,
+#                  to = endDateTicker,
+#                  get = "stock.prices")
+# # we extract close price
+# stocksData <- stocksData %>% 
+#   select(date, symbol, value = close) %>% 
+#   group_by(date, symbol) %>% 
+#   summarise(value = mean(value)) %>% 
+#   arrange(desc(date))
+# stocksData_ts <- stocksData %>% 
+#   spread(key=symbol, value = value) %>% mutate(countryCode = NA) %>% 
+#   arrange(desc(date))
 
 head(stocksData)
 head(stocksData_ts)
@@ -45,7 +60,9 @@ print("Stocks values extraction process FINISHED")
 
 library(tidyquant)
 library(tidyverse)
+library(zoo)
 
+chosenTickers <- c("^VIX", "^VVIX", "GC=F")
 
 # https://www.investopedia.com/5-volatility-indicators-are-warnings-sign-to-the-markets-4584057
 # INDICATORS WE NEED:
@@ -69,22 +86,24 @@ library(tidyverse)
 # https://www.jstor.org/stable/41739216
 tq_transmute_fun_options()
 
-from = today() - years(3)
-stock <- tq_get("^VIX", get = "stock.prices", from = from)
 
-# MOVING AVERAGE
-stock <- stock %>%
-  tq_mutate(select = close, mutate_fun = EMA, n = 50) %>% arrange(desc(date))
+endDateTicker <- today()
+startingDateTicker <- "1900-01-01"
+stocksData <- 
+  tq_get(chosenTickers, from = startingDateTicker, to = endDateTicker, get = "stock.prices") %>% 
+  mutate(close = na_locf(stocksData$close)) %>% 
+  tq_mutate(select = close, mutate_fun = EMA, n = 50, col_rename = "MA") %>% 
+  tq_mutate(select = close, mutate_fun = volatility, calc="close", col_rename = "V") %>% 
+  mutate(HL = (high - low))
+stocksData %>% arrange(desc(date))
 
-# VOLATILITY
-stock <- stock %>%
-  tq_mutate(select = close, mutate_fun = volatility) %>% arrange((date))
+
 
 # Draw Graph 2x2
-matplot(stock$value, axes=FALSE, type=c('l'), col = c('blue'), main = names(stock$value))
+matplot(stocksData$V, axes=FALSE, type=c('l'), col = c('blue'), main = names(stocksData$value))
 axis(2) # show y axis
-axis(1, at=seq_along(1:nrow(stock)),
-     labels=stock$date, las=2)
+axis(1, at=seq_along(1:nrow(stocksData)),
+     labels=stocksData$date, las=2)
 
 
 
