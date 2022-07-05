966350888cbcf99dbfba9361d84c115678f6304b
SUAREZ <Juan-Eloy.Suarez@dupont.com>
2022-07-05 20:04:13 +0200

    upl

--- test_EDA.R
+++ test_EDA.R
@@ -10,5 +10,28 @@ names(df_planetMood)
 # https://rpubs.com/riazakhan94/arima_with_example
 
 data <- df_planetMood[,c("date","VIX")] %>% arrange(date)
-data = ts(data[,2],start = c(2017,1),frequency = 7)
-plot(data, xlab='Year on sale', ylab = 'Number of Textile sold')
+data <- ts(data[,2]+30, start = c(2017,1,1),frequency = 365.25)
+plot(data, xlab='Date', ylab = 'VIX')
+# Step-2
+plot(diff(data),ylab='Differenced VIX')
+# Step-3: Carrying Log Transform Data
+plot(log10(data),ylab='Log (VIX)')
+# Step-4: Difference value
+plot(diff(log10(data)),ylab='Differenced Log (VIX)')
+# Step-5: Evaluate and iterate
+require(forecast)
+ARIMAfit = auto.arima(log10(data), approximation=FALSE,trace=FALSE)
+summary(ARIMAfit)
+# Series: log10(data)
+# ARIMA(0,1,1)(1,1,0)[12] Coefficients:
+#   ma1 sar1
+# -0.5618 -0.6057
+# s.e. 0.1177 0.1078
+# sigma^2 estimated as 0.000444: log likelihood=142.17
+# AIC=-278.34 AICc=-277.91 BIC=-272.11
+# Training set error measures:
+#   ME RMSE MAE MPE
+# Training set 0.000213508 0.01874871 0.01382765 0.009178474
+# MAPE MASE ACF1
+# Training set 0.5716986 0.2422678 0.0325363
+# Step-6 : To examine P and Q values we need to execute acf() and pacf() which is an autocorrelation function.
