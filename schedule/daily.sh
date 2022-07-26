f5d4c8aee06d778772d9255b070784ffc716bb0b
databellum-ai <jes@databellum-ai.com>
2022-07-26 23:26:41 +0000

    upl

---
+++ schedule/daily.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+Rscript ../project2_main/extractDataUptodate.R
+Rscript ../project2_main/sendEmail.R
+
+## OTRAS FORMAS DE LLAMAR DESDE $:
+# < a.R --no-save
+# R < a.R --vanilla
+# R < a.R --save
+# R CMD BATCH a.R
+
+# CRONTAB FOR SCHEDULING TASKS:
+# =============================
+# -View current tasks scheduled:
+# $ crontab -l
+# 
+# -View current tasks scheduled for a user:
+# $ crontab -u [user] -l
+#   (view other commands: https://phoenixnap.com/kb/how-to-list-display-view-all-cron-jobs-linux)
+# 
+# -Edit crontab configuration for current user:
+# $ crontab -e
+#   (this opens editor vim (see commands: https://cambiatealinux.com/comandos-basicos-para-sobrevivir-al-editor-vim)
+# 
+# -The /etc/crontab file (root user) can be edited using a text editor like nano (easier):
+# $ sudo nano /etc/crontab
+#   
+# -Code used for daily run of script .sh:
+# 0 5,17 * * * /scripts/script.sh  
+#   */10 * * * * /home/rstudio/R/climatrade/schedule/daily.sh
+#   (example command will execute at 5 AM and 5 PM daily. You can specify multiple time stamps by comma-separated)
+#   (see other examples: https://tecadmin.net/crontab-in-linux-with-20-examples-of-cron-schedule/)
+# (!) It is important to setup EXECUTE permission (chmod) to the .sh file scheduled
