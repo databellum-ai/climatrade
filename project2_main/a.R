bce0b9afa550e8bea582dbc561b3aee26c604a2e
databellum-ai <jes@databellum-ai.com>
2022-07-26 16:27:15 +0000

    upl

---
+++ project2_main/a.R
@@ -0,0 +1,5 @@
+#!/usr/bin/env Rscript
+sayHello <- function(){
+  print('hello')
+}
+sayHello()
---
+++ project2_main/run_bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Rscript sendEmail.R
+## OTRAS FORMAS DE LLAMAR DESDE $:
+# < a.R --no-save
+# R < a.R --vanilla
+# R < a.R --save
+# R CMD BATCH a.R
